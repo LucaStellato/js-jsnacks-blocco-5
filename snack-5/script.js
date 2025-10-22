0c7ed41dd7406084c5822eb8affa8005944112b6
LucaStellato <lucastellato435@gmail.com>
2025-10-22 14:16:54 +0200

    snack 5 complete with filter

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,7 +2,7 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
-const evenNum=[]
+//const evenNum=[]
 /*for(let i=0; i<nums.length; i++){
     const num=nums[i]
     if(num%2==0){
@@ -11,10 +11,21 @@ const evenNum=[]
 }
 console.log(evenNum)*/
 
-nums.forEach((num)=>{
+/*nums.forEach((num)=>{
     if(num%2==0){
         evenNum.push(num)
     }
 })
-console.log(evenNum)
+console.log(evenNum)*/
+
+const evenNumb= nums.filter((num)=>{
+    if(num%2==0){
+        return true
+    }
+    return false
+})
+
+console.log(evenNumb)
+
+
   
