35f3c5e5686b03b150338b012cde5c44bc484c02
LucaStellato <lucastellato435@gmail.com>
2025-10-22 13:51:44 +0200

    snake 3 complete with forEach

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,9 +2,16 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
-const number=[]
-for(let  i=0 ; i<numbers.length; i++){
+const numberArray=[]
+/*for(let  i=0 ; i<numbers.length; i++){
    number.push(numbers[i]+1)
    
 }
-console.log(number)
+console.log(number)*/
+
+
+numbers.forEach(number => {
+    numberArray.push(number+1)
+    
+});
+console.log(numberArray)
